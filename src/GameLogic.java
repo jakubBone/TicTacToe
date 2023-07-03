548d296b3cc8773b8c418526774fa580c368adb5
jakubBone <jakub.bone@poczta.onet.pl>
2023-07-03 07:31:57 +0200

    Commit 4: Refactered methods with game logic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -1,64 +1,87 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.util.Random;
+import java.io.IOException;
 
-public class GameLogic {
+public class GameLogic  {
     char[][] grid = new char[3][3];
-    char userMark = 'O';
-    char computerMark = 'X';
-    Random random = new Random();
+    Random random;
+    boolean isGameOver = false;
 
-    void computerMove(int column, int row) {
+    char currentPlayer = 'O';
+    BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+
+    ///////
+    String column1 = " ";
+    int column = 0;
+    int row = 0;
+
+
+
+
+    void start() throws IOException {
+        printRules();
+        genererateGrid();
+        playGame();
+
+    }
+
+    void playGame() throws IOException {
+        while (!isGameOver) {
+            printGrid();
+            System.out.println("Please type a column: ");
+            column1 = reader.readLine();
+            System.out.println("Please type a row: ");
+            row = Integer.parseInt(reader.readLine());
+            switch (column1) {
+                case ("A"):
+                    column = 0;
+                    break;
+                case ("B"):
+                    column = 1;
+                    break;
+                case ("C"):
+                    column = 2;
+            }
+            assignCoordinates();
+            switchPlayer();
+            computerMove();
+            switchPlayer();
+            //isGameOver = true;
+        }
+    }
+    void computerMove() {
         System.out.println("Ok, now my move!");
         System.out.println();
-        while (grid[row][column] == computerMark){
-            if(grid[column][row] == '-') {
-                column = random.nextInt(3);
-                row = random.nextInt(3);
-                grid[row][column] = computerMark;
-            }
-        }
+        //while (grid[row][column] == 'X'){
+            column = random.nextInt(3);
+            row = random.nextInt(3);
+            assignCoordinates();
+            printGrid();
     }
-
-    void assignCoordinates(int column, int row) {
+            /*void assignComputerCoordinates() {
         if (column >= 0 && column <= 2 && row >= 0 && row <= 2) {
-            grid[row][column] = userMark;
-            System.out.println();
+            if(grid[column][row] == '-' && grid[column][row] != 'O')
+                grid[row][column] = computerMark;
         }
+        System.out.println();
+    }*/
+
+
+
+
+    void assignCoordinates() {
+        if (column >= 0 && column <= 2 && row >= 0 && row <= 2) {
+                grid[row][column] = currentPlayer;
+                System.out.println();
+        }
+
+    }
+    void switchPlayer(){
+        currentPlayer = (currentPlayer == 'O') ? 'X' : 'O';
     }
 
-
-/*
-            if(userType.equals("A0"))
-                grid[0][0] = 'O';
-            else if (userType.equals("B0"))
-                grid[0][1] = 'O';
-            else if (userType.equals("C0"))
-            grid[0][2] = 'O';
-            else if(userType.equals("A1"))
-            grid[1][0] = 'O';
-            else if (userType.equals("B1"))
-            grid[1][1] = 'O';
-            else if (userType.equals("C1"))
-            grid[1][2] = 'O';
-            else if (userType.equals("A2"))
-            grid[2][0] = 'O';
-            else if (userType.equals("B2"))
-            grid[2][1] = 'O';
-            else if (userType.equals("C2"))
-            grid[2][2] = 'O';
-            else
-            System.out.println("Incorrect answer, try again...");
-        }*/
-
-
-    void genererateGrid() {
-        for (int i = 0; i < 3; i++) {
-            for (int j = 0; j < 3; j++) {
-                grid[i][j] = '-';
-            }
-        }
-    }
-
-    void printGrid() {
+    void printGrid() {                                          // drukuje aktualną tablicę
         System.out.println("  A B C");
         for (int i = 0; i < 3; i++) {
             System.out.print(i + " ");
@@ -69,10 +92,26 @@ public class GameLogic {
         }
     }
 
-    void printRules() {
+    void genererateGrid() {                                     // generuje tablicę
+        for (int i = 0; i < 3; i++) {
+            for (int j = 0; j < 3; j++) {
+                grid[i][j] = '-';
+            }
+        }
+    }
+
+
+
+    void printRules() {                                          // drukuje zasady
+        System.out.print("Hello dude! Welcome in Tic-Tac-Toe game. Please type your name: ");
+        //String name = scanner.nextLine();
+        //System.out.println("Hello " + name + "! My name is Computer" + " Let's start the game!");
+        System.out.println();
         System.out.println("Rules: In this game you need... So, let's play! ");
         System.out.println();
     }
+
+
 
 }
 
--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,53 +1,13 @@
-import java.util.Random;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class TicTacToe {
 
-    public static void main(String[] args) {
-        GameLogic game = new GameLogic();
-        Scanner scanner = new Scanner(System.in);
-        Random random = new Random();
-
-        int column = 0;
-        int row = 0;
-        String column1 = " ";
-
-        System.out.print("Hello dude! Welcome in Tic-Tac-Toe game. Please type your name: ");
-        //String name = scanner.nextLine();
-        //System.out.println("Hello " + name + "! My name is Computer" + " Let's start the game!");
-        System.out.println();
-
-        game.genererateGrid();
-        game.printRules();
-        game.printGrid();
-
-            System.out.println("Please type a column: ");
-            column1 = scanner.nextLine();
-            System.out.println("Please type a row: ");
-            row = scanner.nextInt();
-            switch (column1) {
-                case ("A"):
-                    column = 0;
-                    break;
-                case ("B"):
-                    column = 1;
-                    break;
-                case ("C"):
-                    column = 2;
-            }
-        game.assignCoordinates(column, row);
-        game.printGrid();
-        game.computerMove(column, row);
-
-
-
-
-        // ruch kompa
-
-
-        game.printGrid();
+    public static void main(String[] args) throws IOException {
+        GameLogic gameLogic = new GameLogic();
+        gameLogic.start();
+        }
     }
-}
+
 
 
 
